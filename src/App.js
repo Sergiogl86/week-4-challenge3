c82f4536b088ea6adf2662ae884f6f1ac063b7ff
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-20 18:19:12 +0200

    Siguen los problemas del deploy

--- src/App.js
+++ src/App.js
@@ -48,13 +48,13 @@ function App() {
             <span className="number">667359961</span>
             {/* <!-- El botón de llamar debe tener la clase "activo" cuando -->
           <!-- el número de teléfono tiene 9 cifras --> */}
-            <a href="#" className="call">
+            {/* <a href="#" className="call">
               Call
-            </a>
+            </a> */}
             {/* <!-- Sólo se tiene que ver un botón u otro --> */}
-            <a href="#" className="hang active">
+            {/* <a href="#" className="hang active">
               Hang
-            </a>
+            </a> */}
           </div>
         </main>
       </div>
