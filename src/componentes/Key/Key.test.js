0707e93da45599f2d6726540554045ad642291b9
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-22 08:56:34 +0200

    Se amplia el test de la Key

--- src/componentes/Key/Key.test.js
+++ src/componentes/Key/Key.test.js
@@ -5,25 +5,50 @@ import Key from "./Key";
 
 describe("Given a Key component", () => {
   describe("When it receives a Key value and function", () => {
-    test("Then it should call the recived function", () => {});
-    const setNumeroTelefono = jest.fn();
-    const setCallState = jest.fn();
-    const numeroTelefono = "12345678";
-    const hangState = false;
-    const keyValue = 4;
+    test("Then it should call the recived function", () => {
+      const setNumeroTelefono = jest.fn();
+      const setCallState = jest.fn();
+      const numeroTelefono = "12345678";
+      const hangState = false;
+      const keyValue = 5;
 
-    render(
-      <Context.Provider
-        value={{ setNumeroTelefono, numeroTelefono, setCallState, hangState }}
-      >
-        <Key key={keyValue} numero={keyValue} />
-      </Context.Provider>
-    );
+      render(
+        <Context.Provider
+          value={{ setNumeroTelefono, numeroTelefono, setCallState, hangState }}
+        >
+          <Key key={keyValue} numero={keyValue} />
+        </Context.Provider>
+      );
 
-    const KeyButton = screen.getByRole("button");
-    screen.debug();
-    userEvent.click(KeyButton);
+      const KeyButton = screen.getByRole("button");
+      screen.debug();
+      userEvent.click(KeyButton);
 
-    expect(setCallState).toHaveBeenCalled();
+      expect(setCallState).toHaveBeenCalled();
+    });
+  });
+  describe("When it receives a Key value 5 and function", () => {
+    test("Then it should put 5 in the text button", () => {
+      const setCallState = jest.fn();
+      const numeroTelefono = "12345678";
+      const setNumeroTelefono = jest.fn();
+      const hangState = false;
+      const keyValue = "5";
+
+      render(
+        <Context.Provider
+          value={{ setNumeroTelefono, numeroTelefono, setCallState, hangState }}
+        >
+          <Key key={keyValue} numero={keyValue} />
+        </Context.Provider>
+      );
+
+      const KeyButton = screen.getByRole("button");
+      screen.debug();
+
+      const result = KeyButton.innerHTML;
+
+      expect(result).toEqual(keyValue);
+    });
   });
 });
